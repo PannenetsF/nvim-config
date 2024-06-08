4a5b38b60f87fd834da98d1e5ba47fe6e2bf5602
Pannenets F <pannenets.f@foxmail.com>
2024-06-09 01:08:16 +0800

    fix osc52  halt

--- lua/config/plugins/tools/telescope.lua
+++ lua/config/plugins/tools/telescope.lua
@@ -8,6 +8,13 @@ M.normal_key_map = {
 		g = { telescope.live_grep, "Live Grep" },
 		b = { telescope.buffers, "Buffers" },
 		h = { telescope.help_tags, "Help Tags" },
+		c = { telescope.git_branches, "Checkout branch" },
+		l = { "<cmd>Telescope resume<cr>", "Resume last search" },
+		H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
+		r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+		R = { "<cmd>Telescope registers<cr>", "Registers" },
+		k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+		C = { "<cmd>Telescope commands<cr>", "Commands" },
 		p = {
 			"<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<cr>",
 			"Colorscheme with Preview",
