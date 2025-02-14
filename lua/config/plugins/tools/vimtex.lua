efe0bf03d2aeae69acd497e8b0b9807e462c7230
pf <pannenets.f@foxmail.com>
2025-02-14 17:50:49 +0800

    add vim tex

--- lua/config/plugins/tools/vimtex.lua
+++ lua/config/plugins/tools/vimtex.lua
@@ -4,6 +4,16 @@ local config_shared = function()
 	vim.g.vimtex_compiler_latexmk_engines = {
 		_ = "-pdflatex",
 	}
+	vim.g.vimtex_compiler_latexmk = {
+		options = {
+			"-pdf",
+			"-verbose",
+			"-file-line-error",
+			"-synctex=1",
+			"-interaction=nonstopmode",
+			"--shell-escape",
+		},
+	}
 	vim.g.tex_comment_nospell = 1
 	vim.g.vimtex_compiler_progname = "nvr"
 	-- vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
